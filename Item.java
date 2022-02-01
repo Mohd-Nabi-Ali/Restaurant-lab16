f551fa69758b6e4234cd375f7c13f2c5b58a017c
Mohd Nabi Ali <83170926+Mohd-Nabi-Ali@users.noreply.github.com>
2022-02-01 18:34:21 +0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,7 @@
+package lab16;
+
+public interface Item {
+    double getCost();
+    String getName();
+    String getDescription();
+}
---
+++ OrderAlreadyAddedException.java
@@ -0,0 +1,8 @@
+package lab16;
+
+public class OrderAlreadyAddedException extends Throwable {
+    public OrderAlreadyAddedException(String s) {
+        super(s);
+    }
+}
+
